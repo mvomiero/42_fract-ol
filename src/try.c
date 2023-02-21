a7c07ab24e2afd24090f3aadca3bf79c15929a3e
Marco Vomiero <mvomiero@student.42berlin.de>
2023-02-21 15:47:22 +0100

    some other try

--- src/try.c
+++ src/try.c
@@ -6,13 +6,13 @@
 /*   By: mvomiero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:15:46 by mvomiero          #+#    #+#             */
-/*   Updated: 2023/02/20 18:41:00 by mvomiero         ###   ########.fr       */
+/*   Updated: 2023/02/21 15:46:15 by mvomiero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(void)
+/* int	main(void)
 {
 	void	*mlx;
 	void	*win;
@@ -28,4 +28,29 @@ int	main(void)
 		mlx_pixel_put(mlx, win, i, HEIGHT/2, 0xFFFFFF);
 	}
   	mlx_loop(mlx);
+} */
+
+
+// to use a whatever key to close the opened window
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	ft_close(int keycode, t_vars *vars)
+{
+	(void)keycode;
+	mlx_destroy_window(vars->mlx, vars->win);
+	return (0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	mlx_hook(vars.win, 2, 1L<<0, ft_close, &vars);
+	mlx_loop(vars.mlx);
 }
